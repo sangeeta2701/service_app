06da9a026f39f7b66cdbc79ec64c2858be837fe7
DELL <anjugupta7060@gmail.com>
2023-01-20 19:18:29 +0530

    Servicing app

---
+++ android/app/src/main/java/com/example/service_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.service_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:service_app/Screens/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
