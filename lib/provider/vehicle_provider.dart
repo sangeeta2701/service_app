5f026e61f15588fed0631f8189aabdfd7a689740
rooshannahak <roshannahak1122@gmail.com>
2023-04-01 22:46:31 +0530

    add provider on vehicle page

--- lib/provider/vehicle_provider.dart
+++ lib/provider/vehicle_provider.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:service_app/api/api_client.dart';
+import 'package:service_app/model/vehicleListData.dart';
 
-class VehicleProvider extends ChangeNotifier{}
+class VehicleProvider extends ChangeNotifier {
+  List<Vehicle> _vehicleList = [];
+  List<Vehicle> get getVehicleList => _vehicleList;
+  int get vehicleListCount => _vehicleList.length;
+
+  void fetchVehicleList() async {
+    try {
+      var response = await ApiClient.getServices().fetchVehicleListApi();
+      VehicleListData vehicleListData = vehicleListDataFromJson(response);
+      _vehicleList = vehicleListData.vehicles!;
+    } catch (e) {
+      print("error no 2: $e");
+    }
+    notifyListeners();
+  }
+}
